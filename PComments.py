c6488349f4cbab22ddf623d71d8224d636a8d9a2
Dipak Bari <dipakbari4@gmail.com>
2020-07-17 00:57:08 +0530

    Python comment system

---
+++ PComments.py
@@ -0,0 +1,11 @@
+print("This program only demonstrates the comments section in Python!")
+
+#This is a general comment in python basically ignored by python interpreter
+def sayHello():
+    print("Hello")
+
+def main():
+    sayHello()
+
+if __name__ == "__main__":
+    main()
