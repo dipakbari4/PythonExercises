e38c0d5ee7f88363f8f725095ad8adb7b897a8b4
Dipak Bari <dipakbari4@gmail.com>
2020-07-17 00:52:19 +0530

    Indentation and their scope value in Python

---
+++ PIndentations.py
@@ -0,0 +1,7 @@
+print("Demonstrates the indentations and their values in Python!")
+
+def main():
+    print("Inside the main function")   #This will called later
+print("Doesn't relate with main function")  #This will called first at executioin
+
+if __name__ == "__main__": main()   #executing the main function
