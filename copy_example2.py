77bdfa237c412695f09425d4752a43b4944d0fea
Dipak Bari <dipakbari4@gmail.com>
2021-01-29 21:17:28 +0530

    This time class will call a function from its object

---
+++ copy_example2.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def printDetails(self):
+        print(dipak.name)
+        print(dipak.age)
+
+
+dipak = Person("Dipak", 27)
+dipak.printDetails()
